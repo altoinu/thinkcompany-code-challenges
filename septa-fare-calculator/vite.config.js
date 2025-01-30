c04968c8630f0bb1786e30f7ff1195720f79bb9c
Kaoru Kawashima <8354804+altoinu@users.noreply.github.com>
2025-01-29 19:31:49 -0500

    vite config update for supported browsers

--- septa-fare-calculator/vite.config.js
+++ septa-fare-calculator/vite.config.js
@@ -4,4 +4,14 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Browserlist configuration for modern browsers
+  browserslist: [
+    "last 2 versions", // Last 2 versions of each browser
+    "Edge >= 80", // Microsoft Edge version 80 and above
+    "Chrome >= 80", // Google Chrome version 80 and above
+    "Firefox >= 70", // Firefox version 70 and above
+    "Safari >= 13", // Safari version 13 and above (covers iOS)
+    "Android >= 80", // Chrome for Android (approximated using Android version)
+  ],
 });
